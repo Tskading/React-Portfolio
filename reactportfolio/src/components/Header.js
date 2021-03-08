bed795bd94706076511b4029d23a79415a39dba9
Tskading <Tskading@gmail.com>
2021-03-08 00:37:29 -0800

    Added projects with live page and github links.

--- reactportfolio/src/components/Header.js
+++ reactportfolio/src/components/Header.js
@@ -3,29 +3,26 @@ import React from "react";
 function Header({}) {
   return (
     <>
-      <div class="jumbotron">
-        <h1 class="display-4">Thomas Samuel Kading</h1>
-        <p class="lead">
-          This is my portfolio - now built with React. This is such a fun way to
-          build and present myself!
-        </p>
-        <hr class="my-4" />
+      <div className="myHeader">
+        <h1 className="display-4">Thomas Samuel Kading</h1>
+
+        <hr className="my-4" />
         <p>Nav-Links</p>
-        <a class="btn btn-primary btn-lg" href="/" role="button">
+        <a className="btn-lg linkBtn" href="/" role="button">
           About Me
         </a>
-        <a class="btn btn-primary btn-lg" href="/Work" role="button">
+        <a className="btn-lg linkBtn" href="/Work" role="button">
           Work
         </a>
-        <a class="btn btn-primary btn-lg" href="/ContactMe" role="button">
+        <a className="btn-lg linkBtn" href="/ContactMe" role="button">
           Contact Me
         </a>
-        <a class="btn btn-primary btn-lg" href="/Resume" role="button">
+        <a className="btn-lg linkBtn" href="/Resume" role="button">
           Resume
         </a>
       </div>
 
-      <div class="hero">
+      <div className="hero">
         <p>
           If you only focus on the fence, what lies beyond will always seem out
           of focus.
--- reactportfolio/src/components/ProjectCard.js
+++ reactportfolio/src/components/ProjectCard.js
@@ -2,13 +2,14 @@ import React from "react";
 
 function ProjectCard(props) {
     return (
-        <div class="card">
-            {console.log(props)};
-  <img src={props.work.img} className="card-img-top" alt="SongBirdie Logo" style={{"width": "18rem"}}/>
-  <div class="card-body">
-    <h5 class="card-title">{props.work.name}</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div className="card">
+            {console.log(props)}
+  <img src={props.work.img} className="card-img-top" alt="Project Screenshot" style={{"width": "auto"}}/>
+  <div className="card-body">
+    <h5 className="card-title">{props.work.name}</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a target="_blank" rel="noreferrer" href={props.work.liveHref} className="btn btn-primary">Live Page</a>
+    <a target="_blank" rel="noreferrer" href={props.work.hubHref} className="btn btn-primary">Github Page</a>
   </div>
 </div>
     )
