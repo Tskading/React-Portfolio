7a84cbc1a99be8d7f512f8c4779438c266c691ba
Tskading <Tskading@gmail.com>
2021-03-09 19:48:27 -0800

    Updated css and img links to refrence repo.

--- reactportfolio/src/App.js
+++ reactportfolio/src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import Header from "./components/Header"
-import ProjectCard from "./components/ProjectCard";
 import About from "./pages/About";
 import Work from "./pages/Work";
 import ContactMe from "./pages/ContactMe";
 import Resume from "./pages/Resume";
 import Footer from "./components/Footer";
-import { Document } from 'react-pdf'
+
 
 function App() {
   const pathName = window.location.pathname;
